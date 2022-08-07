6df661afe56728373eadd904f2a9a541b3ada00c
Joshua Hawks <jdhawks@gmail.com>
2022-08-07 11:36:49 -0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const workoutRoutes = require('./routes/workouts');
 
 // middleware
 app.use(express.json());
+
+// cors
+app.use(
+	cors({ origin: 'https://62efe7ae45349f367639b5c0--workout-mern.netlify.app' })
+);
 
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
